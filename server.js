36fc5c6f8501b8dc7977d06998d9d8e09965e530
Ahsan Shafi <ahsanshafi8@gmail.com>
2022-10-07 19:46:24 +0500

    make it better

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post("/send", function (req, res) {
   });
 });
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
